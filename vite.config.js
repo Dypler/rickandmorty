d0632fd2647e1bbaec8742146cb2b890a5e8eb76
Saveliy Suntsov <sun.zakup@mail.ru>
2024-10-07 13:05:15 +0400

    Правка

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
@@ -12,11 +11,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['axios'], // добавьте это, если возникли проблемы с axios
     },
   },
 });
